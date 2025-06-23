53881ff067bcabb5bc5ca854171ce52ade3e37eb
amansiwach868 <amansiwach868@gmail.com>
2025-06-23 21:27:55 +0530

    Featured Categories complete

--- src/components/FeaturedCategories.jsx
+++ src/components/FeaturedCategories.jsx
@@ -1,23 +1,46 @@
 import React from 'react'
+import { FEATURED_CATEGORIES_PRODUCTS } from '../utils/hepler';
+import HeadingAndPara from './common/HeadingAndPara'
 
 const FeaturedCategories = () => {
     const styles = [
-        'col-start-1 col-end-3 row-start-1 row-end-2 bg-[#D5E8FF]',
-        'col-start-3 col-end-4 row-start-1 row-end-2 bg-[#EEF4FB]',
-        'col-start-4 col-end-5 row-start-1 row-end-2 bg-[#E5E4E2]',
-        'col-start-1 col-end-2 row-start-2 row-end-3 bg-[#EEF4FB]',
-        'col-start-2 col-end-3 row-start-2 row-end-3 bg-[#E5E4E2]',
-        'col-start-3 col-end-5 row-start-2 row-end-3 bg-[#D5E8FF]'
+        'lg:col-start-1 lg:col-end-3 lg:row-start-1 lg:row-end-2 sm:row-start-1 sm:row-end-2 sm:col-start-1 sm:col-end-2 bg-[#D5E8FF]',
+        'lg:col-start-3 lg:col-end-4 lg:row-start-1 lg:row-end-2 sm:row-start-1 sm:row-end-2 sm:col-start-2 sm:col-end-3 bg-[#EEF4FB]',
+        'lg:col-start-4 lg:col-end-5 lg:row-start-1 lg:row-end-2 sm:row-start-2 sm:row-end-3 sm:col-start-1 sm:col-end-3 bg-[#E5E4E2]',
+        'lg:col-start-1 lg:col-end-2 lg:row-start-2 lg:row-end-3 sm:row-start-3 sm:row-end-4 sm:col-start-1 sm:col-end-2 bg-[#EEF4FB]',
+        'lg:col-start-2 lg:col-end-3 lg:row-start-2 lg:row-end-3 sm:row-start-3 sm:row-end-4 sm:col-start-2 sm:col-end-3 bg-[#E5E4E2]',
+        'lg:col-start-3 lg:col-end-5 lg:row-start-2 lg:row-end-3 sm:row-start-4 sm:row-end-5 sm:col-start-1 sm:col-end-3 bg-[#D5E8FF]'
     ];
+    const imgStyle = [
+        'sm:max-w-[372px] max-w-[246px] right-0 bottom-0 w-full ml-auto sm:min-h-[261px]',
+        'sm:max-w-[250px] max-w-[157px] right-1/2 lg:translate-x-1/2 -bottom-6 w-full sm:min-h-[213px]',
+        'sm:max-w-[184px] max-w-[162px] right-1/2 lg:translate-x-1/2 bottom-3 w-full sm:min-h-[208px]',
+        'sm:max-w-[306px] max-w-[294px] sm:left-0 sm:bottom-0 w-full sm:min-h-[261px] absolute max-sm:-rotate-90 -right-4 bottom-4',
+        'sm:max-w-[266px] max-w-[198px] right-1/2 lg:translate-x-1/2 bottom-[18px] w-full sm:min-h-[187px]',
+        'right-0 top-0 bottom-0 left-0 w-full z-0',
+    ]
+    const headingStyle = [
+        'max-w-[172px] w-full xl:ml-[62px] sm:ml-[30px] xl:mt-[117px] mr-auto ml-6 mt-6' ,
+        'max-w-[213px] w-full sm:mb-0 mb-[46px] sm:mt-8 text-center mx-auto',
+        'w-full sm:mb-0 mb-[39px] mt-[34px] text-center',
+        'max-w-[196px] sm:ml-auto w-full sm:!absolute sm:mt-[26px] sm:mr-4 top-6 left-6',
+        'w-full sm:mb-0 mb-[44px] text-center sm:mt-2.5',
+        'text-center sm:mb-0 mb-[50px] sm:mt-12',
+    ]
     return (
-        <div>
-            <div className="grid grid-cols-4 grid-rows-2 gap-6">
-                <div className="col-start-1 col-end-3 row-start-1 row-end-2 bg-red-200">Div 1</div>
-                <div className="col-start-3 col-end-4 row-start-1 row-end-2 bg-blue-200">Div 2</div>
-                <div className="col-start-4 col-end-5 row-start-1 row-end-2 bg-green-200">Div 3</div>
-                <div className="col-start-1 col-end-2 row-start-2 row-end-3 bg-yellow-200">Div 4</div>
-                <div className="col-start-2 col-end-3 row-start-2 row-end-3 bg-pink-200">Div 5</div>
-                <div className="col-start-3 col-end-5 row-start-2 row-end-3 bg-purple-200">Div 6</div>
+        <div className='max-w-[1384px] px-6 mx-auto my-[132px]'>
+            <HeadingAndPara heading={'Featured Categories'} para={'Shop Our Most Popular Products'} headingClass={'text-[20px] font-normal italic text-[#112D49] text-center w-full mb-2 leading-[120%]'} paraClass={'sm:text-[48px] text-[14px] max-sm:max-w-[264px] font-bold text-[#112D49] text-center max-w-[602px] mx-auto w-full mb-[57px]'} />
+            <div className="grid lg:grid-cols-4 lg:grid-rows-2 sm:grid-cols-2 sm:grid-rows-4 grid-cols-1 gap-6">
+                {
+                    FEATURED_CATEGORIES_PRODUCTS.map((item, i) => {
+                        return <div key={i} className={`${styles[i]} lg:block flex flex-col-reverse gap-4 sm:min-h-[335px] relative rounded-[8px] cursor-pointer active:scale-95 duration-200 overflow-hidden items-center min-h-[298px]`}>
+                            <div className={`lg:absolute ${imgStyle[i]}`}>
+                                <img className={`object-cover h-full w-full md:rounded-[30px] sm:rounded-[20px] rounded-[10px] sm:absolute`} src={item.categorieImg} alt='img' />
+                            </div>
+                            <h4 className={`md:text-[32px] sm:text-[28px] text-[24px] font-bold text-[#112D49] relative z-10 ${headingStyle[i]}`}>{item.categorieName}</h4>
+                        </div>
+                    })
+                }
             </div>
         </div>
     )
--- src/utils/hepler.js
+++ src/utils/hepler.js
@@ -1,28 +1,32 @@
-import categorieImg1 from '../assets/img/svg/categorieImg-1.png'
+import categorieImg1 from '../assets/img/png/categorieImg-1.png'
+import categorieImg2 from '../assets/img/png/categorieImg-2.png'
+import categorieImg3 from '../assets/img/png/categorieImg-3.png'
+import categorieImg4 from '../assets/img/png/categorieImg-4.png'
+import categorieImg5 from '../assets/img/png/categorieImg-5.png'
+import categorieImg6 from '../assets/img/png/categorieImg-6.png'
 export const FEATURED_CATEGORIES_PRODUCTS = [
     {
         categorieName: 'Sensors & Modules',
         categorieImg: categorieImg1,
-        class:
     },
     {
-        categorieName: 'Sensors & Modules',
-        categorieImg: categorieImg1
+        categorieName: 'Custom PC Parts',
+        categorieImg: categorieImg2
     },
     {
-        categorieName: 'Sensors & Modules',
-        categorieImg: categorieImg1
+        categorieName: 'DIY Tools',
+        categorieImg: categorieImg3
     },
     {
-        categorieName: 'Sensors & Modules',
-        categorieImg: categorieImg1
+        categorieName: 'Cables & Connectors',
+        categorieImg: categorieImg4
     },
     {
-        categorieName: 'Sensors & Modules',
-        categorieImg: categorieImg1
+        categorieName: 'Gaming Peripherals',
+        categorieImg: categorieImg5
     },
     {
-        categorieName: 'Sensors & Modules',
-        categorieImg: categorieImg1
+        categorieName: 'Raspberry Pi Kits',
+        categorieImg: categorieImg6
     },
 ]
