03ad5139ae176e36df37901f0bfcdfe51e5abe8a
amansiwach868 <amansiwach868@gmail.com>
2025-06-28 16:27:10 +0530

    bloge page spacing solved

--- src/components/BlogPage.jsx
+++ src/components/BlogPage.jsx
@@ -6,26 +6,26 @@ import CustomButton from './common/CustomButton';
 
 const BlogPage = () => {
     const styles = [
-        'sm:row-start-1 sm:row-end-2 sm:col-start-1 sm:col-end-2 bg-[#E5E4E2] ',
+        'sm:row-start-1 sm:row-end-2 sm:col-start-1 sm:col-end-2 bg-[#E5E4E2] px-4',
         'sm:row-start-1 sm:row-end-2 sm:col-start-2 sm:col-end-3 bg-[#EEF4FB] ',
-        'sm:row-start-2 sm:row-end-3 sm:col-start-1 sm:col-end-3 bg-[#D5E8FF]  flex justify-center items-center'
+        'sm:row-start-2 sm:row-end-3 sm:col-start-1 sm:col-end-3 bg-[#D5E8FF]  flex justify-center items-center px-4'
     ];
 
     const imgStyles = [
         'xl:!max-w-[281px] max-w-[202px] xl:min-h-[235px] lg:right-[6px] lg:bottom-[3px] max-lg:top-[10px] max-lg:left-1/2 max-lg:-translate-x-1/2',
         'lg:!max-w-[197px] max-w-[157px] lg:min-h-[358px] lg:top-0 lg:right-0 max-lg:-rotate-90 max-lg:-top-13 max-lg:left-1/2 max-lg:-translate-x-1/2',
-        'xl:!max-w-[375px] lg:max-w-[300px] max-w-[162px] xl:min-h-[303px] lg:left-[18px] lg:bottom-1/2 lg:translate-y-1/2 max-lg:top-0 max-lg:left-0'
+        'xl:!max-w-[375px] md:max-w-[300px] sm:max-w-[230px] max-w-[200px] xl:min-h-[303px] lg:left-[18px] lg:bottom-1/2 lg:translate-y-1/2 max-lg:top-0 max-lg:left-0'
     ];
 
     const headingStyles = [
-        '',
-        'max-w-[360px]',
+        'min-[700px]:!max-w-[700px] !max-w-[250px]',
+        'md:max-w-[360px] !max-w-[275px]',
         'text-center'
     ];
 
     const paraStyles = [
-        'max-w-[250px]',
-        'max-w-[360px]',
+        'sm:max-w-[270px] max-w-[250px]',
+        'md:max-w-[360px] max-w-[250px]',
         'text-center'
     ];
 
@@ -56,7 +56,7 @@ const BlogPage = () => {
                             alt={item.heading || 'Blog image'}
                         />
                         <img
-                            className={`${item.active ? 'flex' : 'hidden'} pointer-events-none object-cover w-full lg:rounded-[30px] sm:rounded-[20px] rounded-[10px] absolute xl:!max-w-[365px] lg:max-w-[280px] max-w-[162px] xl:min-h-[303px] lg:right-[27px] lg:bottom-1/2 lg:translate-y-1/2 bottom-0 right-0`}
+                            className={`${item.active ? 'flex' : 'hidden'} pointer-events-none object-cover w-full lg:rounded-[30px] sm:rounded-[20px] rounded-[10px] absolute xl:!max-w-[365px] md:max-w-[280px] sm:max-w-[230px] max-w-[200px] xl:min-h-[303px] lg:right-[27px] lg:bottom-1/2 lg:translate-y-1/2 bottom-0 right-0`}
                             src={item.blogPgaeImg_1}
                             alt={item.heading || 'Blog image'}
                         />
