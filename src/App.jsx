c0951bcb46ccda20fee5096a2d20eac6fc95ce9f
amansiwach868 <amansiwach868@gmail.com>
2025-06-28 11:23:43 +0530

    scroll to top problam solved

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,42 @@ import SignUpPage from './components/SignUpPage';
 import ForgetPassword from './components/ForgetPassword';
 import Home from './pages/Home';
 import CartPage from './components/CartPage';
-import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import CheckOut from './components/CheckOut';
 import ProductDetail from './components/ProductDetail';
 import WishList from './components/WishList';
-import BackToTop from './components/common/BackToTop'
+import BackToTop from './components/common/BackToTop';
 
-import Lenis from 'lenis'
-const lenis = new Lenis({
-  autoRaf: true,
-});
+import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import { useEffect, useRef } from 'react';
+import Lenis from 'lenis';
 
 function App() {
-  const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
   const location = useLocation();
+  const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
   const hideHeaderFooterRoutes = ['/login', '/signup', '/forgetpassword'];
+
+  const lenisRef = useRef(null);
+
+  useEffect(() => {
+    const lenis = new Lenis();
+    lenisRef.current = lenis;
+
+    const raf = (time) => {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    };
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (lenisRef.current) {
+      lenisRef.current.scrollTo(0);
+    }
+  }, [location.pathname]);
 
   return (
     <>
@@ -28,40 +49,17 @@ function App() {
 
       <Routes>
         <Route index element={<Navigate to={isLoggedIn ? '/home' : '/login'} />} />
-        <Route
-          path="/login"
-          element={isLoggedIn ? <Navigate to="/home" /> : <LoginPage />}
-        />
-        <Route
-          path="/signup"
-          element={isLoggedIn ? <Navigate to="/home" /> : <SignUpPage />}
-        />
-        <Route
-          path="/forgetpassword"
-          element={isLoggedIn ? <Navigate to="/home" /> : <ForgetPassword />}
-        />
-        <Route
-          path="/home"
-          element={isLoggedIn ? <Home /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/cart"
-          element={isLoggedIn ? <CartPage /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/productdetails/:slug"
-          element={isLoggedIn ? <ProductDetail /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/checkout"
-          element={isLoggedIn ? <CheckOut /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/wishlist"
-          element={isLoggedIn ? <WishList /> : <Navigate to="/login" />}
-        />
+        <Route path="/login" element={isLoggedIn ? <Navigate to="/home" /> : <LoginPage />} />
+        <Route path="/signup" element={isLoggedIn ? <Navigate to="/home" /> : <SignUpPage />} />
+        <Route path="/forgetpassword" element={isLoggedIn ? <Navigate to="/home" /> : <ForgetPassword />} />
+        <Route path="/home" element={isLoggedIn ? <Home /> : <Navigate to="/login" />} />
+        <Route path="/cart" element={isLoggedIn ? <CartPage /> : <Navigate to="/login" />} />
+        <Route path="/productdetails/:slug" element={isLoggedIn ? <ProductDetail /> : <Navigate to="/login" />} />
+        <Route path="/checkout" element={isLoggedIn ? <CheckOut /> : <Navigate to="/login" />} />
+        <Route path="/wishlist" element={isLoggedIn ? <WishList /> : <Navigate to="/login" />} />
         <Route path="*" element={<Navigate to={isLoggedIn ? "/home" : "/login"} />} />
       </Routes>
+
       {!hideHeaderFooterRoutes.includes(location.pathname) && <Footer />}
       <BackToTop />
     </>
