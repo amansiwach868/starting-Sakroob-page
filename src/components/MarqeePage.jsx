cb2f2ebe4f99ee9a0e3fc138bdbc8bcc955c86f5
amansiwach868 <amansiwach868@gmail.com>
2025-06-30 10:22:45 +0530

    marquee font matched

--- src/components/MarqeePage.jsx
+++ src/components/MarqeePage.jsx
@@ -6,18 +6,18 @@ import { Game, Graphic, Monitors, Smartwatch, Storage, Television } from '../uti
 const MarqeePage = () => {
     return (
         <Marquee className='sm:pt-[27px] pb-6 pt-[21px]'>
-            <MarqeeCards headingClass={'md:text-md  sm:text-[14px] text-[12px] leading-[120%] font-bold text-[#112D49]'} heading={'Storage (ssd)'} children={<Storage />} />
-            <MarqeeCards headingClass={'md:text-md  sm:text-[14px] text-[12px] leading-[120%] font-bold text-[#112D49]'} heading={'Game'} children={<Game />} />
-            <MarqeeCards headingClass={'md:text-md  sm:text-[14px] text-[12px] leading-[120%] font-bold text-[#112D49]'} heading={'Monitors'} children={<Monitors />} />
-            <MarqeeCards headingClass={'md:text-md  sm:text-[14px] text-[12px] leading-[120%] font-bold text-[#112D49]'} heading={'Smartwatch'} children={<Smartwatch />} />
-            <MarqeeCards headingClass={'md:text-md  sm:text-[14px] text-[12px] leading-[120%] font-bold text-[#112D49]'} heading={'Televisions'} children={<Television />} />
-            <MarqeeCards headingClass={'md:text-md  sm:text-[14px] text-[12px] leading-[120%] font-bold text-[#112D49]'} heading={'Graphic Card'} children={<Graphic />} />
-            <MarqeeCards headingClass={'md:text-md  sm:text-[14px] text-[12px] leading-[120%] font-bold text-[#112D49]'} heading={'Storage (ssd)'} children={<Storage />} />
-            <MarqeeCards headingClass={'md:text-md  sm:text-[14px] text-[12px] leading-[120%] font-bold text-[#112D49]'} heading={'Game'} children={<Game />} />
-            <MarqeeCards headingClass={'md:text-md  sm:text-[14px] text-[12px] leading-[120%] font-bold text-[#112D49]'} heading={'Monitors'} children={<Monitors />} />
-            <MarqeeCards headingClass={'md:text-md  sm:text-[14px] text-[12px] leading-[120%] font-bold text-[#112D49]'} heading={'Smartwatch'} children={<Smartwatch />} />
-            <MarqeeCards headingClass={'md:text-md  sm:text-[14px] text-[12px] leading-[120%] font-bold text-[#112D49]'} heading={'Televisions'} children={<Television />} />
-            <MarqeeCards headingClass={'md:text-md  sm:text-[14px] text-[12px] leading-[120%] font-bold text-[#112D49]'} heading={'Graphic Card'} children={<Graphic />} />
+            <MarqeeCards headingClass={'md:text-base sm:text-[14px] text-[12px] leading-[120%] font-bold text-[#112D49]'} heading={'Storage (ssd)'} children={<Storage />} />
+            <MarqeeCards headingClass={'md:text-base sm:text-[14px] text-[12px] leading-[120%] font-bold text-[#112D49]'} heading={'Game'} children={<Game />} />
+            <MarqeeCards headingClass={'md:text-base sm:text-[14px] text-[12px] leading-[120%] font-bold text-[#112D49]'} heading={'Monitors'} children={<Monitors />} />
+            <MarqeeCards headingClass={'md:text-base sm:text-[14px] text-[12px] leading-[120%] font-bold text-[#112D49]'} heading={'Smartwatch'} children={<Smartwatch />} />
+            <MarqeeCards headingClass={'md:text-base sm:text-[14px] text-[12px] leading-[120%] font-bold text-[#112D49]'} heading={'Televisions'} children={<Television />} />
+            <MarqeeCards headingClass={'md:text-base sm:text-[14px] text-[12px] leading-[120%] font-bold text-[#112D49]'} heading={'Graphic Card'} children={<Graphic />} />
+            <MarqeeCards headingClass={'md:text-base sm:text-[14px] text-[12px] leading-[120%] font-bold text-[#112D49]'} heading={'Storage (ssd)'} children={<Storage />} />
+            <MarqeeCards headingClass={'md:text-base sm:text-[14px] text-[12px] leading-[120%] font-bold text-[#112D49]'} heading={'Game'} children={<Game />} />
+            <MarqeeCards headingClass={'md:text-base sm:text-[14px] text-[12px] leading-[120%] font-bold text-[#112D49]'} heading={'Monitors'} children={<Monitors />} />
+            <MarqeeCards headingClass={'md:text-base sm:text-[14px] text-[12px] leading-[120%] font-bold text-[#112D49]'} heading={'Smartwatch'} children={<Smartwatch />} />
+            <MarqeeCards headingClass={'md:text-base sm:text-[14px] text-[12px] leading-[120%] font-bold text-[#112D49]'} heading={'Televisions'} children={<Television />} />
+            <MarqeeCards headingClass={'md:text-base sm:text-[14px] text-[12px] leading-[120%] font-bold text-[#112D49]'} heading={'Graphic Card'} children={<Graphic />} />
         </Marquee>
     )
 }
